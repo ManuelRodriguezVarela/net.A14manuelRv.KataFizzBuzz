8ea3047223c4bbf38167bf91fb42795b2d28870d
ManuelRodriguezVarela <manuel_rodriguez_varela@yahoo.es>
2016-05-15 23:59:29 +0200

    añadido y comprobado el código que si contiene 5 devuelve buzz

--- src/net/iessanclemente/A14ManuelRV/FizzBuzz.java
+++ src/net/iessanclemente/A14ManuelRV/FizzBuzz.java
@@ -7,14 +7,28 @@ public class FizzBuzz {
 		
 		if(number%5==0 && number%3==0)
 			result = "fizzbuzz";
+		
 		else if(number%3==0)
 			result = "fizz";
-		else if(number%5==0)
+		
+		else if(isMultipleOfFive(number)|| isContainsFive(number))
 			result = "buzz";
+		
 		else
 			result = Integer.toString(number);
 		
 		return result;
+	}
+	
+	private static boolean isMultipleOfFive(int number)
+	{
+		return number % 5 == 0;
+	}
+	
+	private static boolean isContainsFive(int number)
+	{
+		String numberName = Integer.toString(number);
+		return numberName.contains("5");
 	}
 
 }
